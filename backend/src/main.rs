0bfc58844123e0199ed1e75fbf3165184731a458
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-03-19 15:15:36 +0300

    compose prod is working 👍

---
+++ backend/src/main.rs
@@ -0,0 +1,16 @@
+use anyhow::Context;
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let router = Router::new().route("/test", get(|| async { "Hello world!" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .context("error bind address")?;
+
+    axum::serve(listener, router)
+        .await
+        .context("error serve app")?;
+
+    Ok(())
+}
